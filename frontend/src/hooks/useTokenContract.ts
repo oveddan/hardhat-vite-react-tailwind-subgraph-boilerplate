41a10669fb79a6de780bf94cc5ff446d64434fcc
Dan Oved <stangogh@gmail.com>
2022-10-05 14:51:44 -0500

    added prettier and format on save

--- frontend/src/hooks/useTokenContract.ts
+++ frontend/src/hooks/useTokenContract.ts
@@ -1,88 +1,65 @@
-import { useEffect, useMemo, useState } from "react";
-import { chain, useSigner } from "wagmi";
-import { useAccount, useContract } from "wagmi";
-import localHostAddress from '../contracts/localhost/contract-address.json'
-import localHostContract from '../contracts/localhost/Token.json'
-import goerliHostAddress from '../contracts/goerli/contract-address.json'
-import goerilHostContract from '../contracts/goerli/Token.json'
-import { Contract, ContractInterface, providers, Signer } from "ethers";
-import { ExtractAbiFunctions } from 'abitype'
-
-
+import { useEffect, useMemo, useState } from 'react';
+import { chain } from 'wagmi';
+import { useAccount } from 'wagmi';
+import localHostAddress from '../contracts/localhost/contract-address.json';
+import localHostContract from '../contracts/localhost/Token.json';
+import goerliHostAddress from '../contracts/goerli/contract-address.json';
+import goerilHostContract from '../contracts/goerli/Token.json';
+import { Contract, ContractInterface } from 'ethers';
 
 type GetContractArgs = {
   /** Contract address or ENS name */
-  addressOrName: string
+  addressOrName: string;
   /** Contract interface or ABI */
-  contractInterface: ContractInterface
+  contractInterface: ContractInterface;
   /** Signer or provider to attach to contract */
-}
+};
 
+const getContractAddressAndAbi = (chainId: number | undefined): GetContractArgs | null => {
+  if (!chainId) return null;
 
+  if (chainId === chain.localhost.id) {
+    return {
+      addressOrName: localHostAddress.Token,
+      contractInterface: localHostContract.abi,
+    };
+  }
 
+  if (chainId === chain.goerli.id) {
+    return {
+      addressOrName: goerliHostAddress.Token,
+      contractInterface: goerilHostContract.abi,
+    };
+  }
 
-const getContractAddressAndAbi = (chainId: number | undefined): GetContractArgs| null => {
-if (!chainId) return null;
+  throw new Error('invalid chain id');
+};
 
-if (chainId === chain.localhost.id) {
-      return {
-        addressOrName: localHostAddress.Token,
-        contractInterface: localHostContract.abi
-      }
-      }
-
-      if (chainId === chain.goerli.id) {
-        return {
-        addressOrName: goerliHostAddress.Token,
-        contractInterface: goerilHostContract.abi
-        }
-      }
-
-      throw new Error("invalid chain id")
-}
-
-export function getContract<T = Contract>({
-  addressOrName,
-  contractInterface,
-}: GetContractArgs) {
-  return <T>(
-    (<unknown>(
-      new Contract(
-        addressOrName,
-        contractInterface,
-      )
-    ))
-  )
+export function getContract<T = Contract>({ addressOrName, contractInterface }: GetContractArgs) {
+  return <T>(<unknown>new Contract(addressOrName, contractInterface));
 }
 
 const useTokenContract = () => {
-
-  const { connector: activeConnector, isConnected } = useAccount()
-
-  const signerResult = useSigner();
+  const { connector: activeConnector, isConnected } = useAccount();
 
   const [contractArgs, setContractArgs] = useState<GetContractArgs | null>(null);
 
   useEffect(() => {
     (async () => {
       const chainId = await activeConnector?.getChainId();
-    
+
       const addressAndAbi = getContractAddressAndAbi(chainId);
-    
+
       setContractArgs(addressAndAbi);
     })();
-  }, [activeConnector, isConnected])
-
+  }, [activeConnector, isConnected]);
 
   const contract = useMemo(() => {
     if (!contractArgs) return null;
-    return getContract<Contract>(
-      contractArgs
-    )
-  }
-  , [contractArgs]);
+    return getContract(contractArgs);
+  }, [contractArgs]);
 
   return contract;
 };
 
-export default useContract;
+export default useTokenContract;
