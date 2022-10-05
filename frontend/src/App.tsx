cf5a48c273d2db728b54270e3be6a26e44492bfc
Dan Oved <stangogh@gmail.com>
2022-10-05 12:20:35 -0500

    can connect to hardhat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { WagmiConfig } from 'wagmi'
 import './App.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import { chains, client } from './web3/client'
+import Web3Login from './web3/Web3Login'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chains}>
+        <Web3Login />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </RainbowKitProvider>
+    </WagmiConfig>
   )
 }
 
--- frontend/src/web3/client.ts
+++ frontend/src/web3/client.ts
@@ -1,34 +1,22 @@
 import { chain, createClient, configureChains } from "wagmi";
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
-const alchemyId = "7JOHPLbiPe75KZ3OBykXYwjZiHwcmX2x";
-
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 const { chains, provider, webSocketProvider } = configureChains(
-  [chain.localhost, chain.goerli],
+  [chain.hardhat, chain.goerli, chain.polygonMumbai],
   [
-    jsonRpcProvider({
-      rpc: (chainToConnect) => {
-        if (chainToConnect.id !== chain.localhost.id) return null
-        return {
-          http: `http://localhost:8545`,
-          webSocket: `wss://localhost:8545`,
-        }
-      }
-    }),
-    alchemyProvider({ apiKey: alchemyId }),
+    alchemyProvider({ apiKey: import.meta.env.VITE_APP_ALCHEMY_ID }),
     publicProvider(),
   ],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "landaxr",
+  appName: import.meta.env.VITE_APP_APP_NAME,
   chains,
 });
 
